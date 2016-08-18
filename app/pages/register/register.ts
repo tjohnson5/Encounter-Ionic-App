61ff08c16e49fdf9aa7f3d04891a2292cb4b8027
tjohnson5 <tjohnson@biggtrav.com>
2016-08-17 21:48:33 -0500

    Add files via upload

--- app/pages/register/register.ts
+++ app/pages/register/register.ts
@@ -1,137 +1,251 @@
 import { Page } from 'ionic-angular';
 import {Component} from "@angular/core";
 import {FormBuilder, ControlGroup, Validators, AbstractControl } from '@angular/common';
-import {NavController, Loading, Platform, Storage, SqlStorage} from 'ionic-angular';
+import {NavController, LoadingController, ToastController, ViewController,ModalController, Platform, Storage, SqlStorage} from 'ionic-angular';
 import {EventRegService} from '../../providers/event-reg-service/event-reg-service';
+import {StudentInfoService} from '../../providers/student-info-service/student-info-service';
+import {GlobalOverlayProvider} from '../../providers/global-overlay-provider/global-overlay-provider';
+import {GlobalObjectProvider} from '../../providers/global-object-provider/global-object-provider';
 import {Http, Headers} from '@angular/http';
+import {ErrorHandler} from '../../providers/error-handler/error-handler';
+import {ModalsContentPage} from '../home/home';
 
 
 @Component({
   templateUrl: 'build/pages/register/register.html',
-  providers: [EventRegService],
+  providers: [EventRegService]
 })
 export class EventRegPage {
-    public events: any;
-    platform: Platform;
-    http: Http;
-    student: any;
-    storage: Storage;
-    formValues: String;
+  data: any;
+  public events: any;
+  platform: Platform;
+  http: Http;
+  student: any;
+  storage: Storage;
+  formValues: String;
+  loading: any;
 
-    studentForm: ControlGroup;
+  studentForm: ControlGroup;
 
-    firstName: AbstractControl;
-    lastName: AbstractControl;
-    email: AbstractControl;
-    phone: AbstractControl;
-    year: AbstractControl;
-    gender: AbstractControl;
+  firstName: AbstractControl;
+  lastName: AbstractControl;
+  email: AbstractControl;
+  phone: AbstractControl;
+  year: AbstractControl;
+  gender: AbstractControl;
 
-    constructor(private nav: NavController, public eventRegService: EventRegService, platform: Platform, builder: FormBuilder, http: Http) {
-        this.http = http;
-        this.platform = platform;
-        this.student = [];
-        this.platform.ready().then(() => {
-            this.storage = new Storage(SqlStorage);
-        });
-        this.presentLoadingDefault();
-          this.loadEvents();
-          this.loadStudentInfo();// Pull the student info out of the database right off the bat and store it in a variable
-    }
+  constructor(private nav: NavController, public eventRegService: EventRegService, public studentInfoService: StudentInfoService, platform: Platform, builder: FormBuilder, http: Http, public viewCtrl: ViewController, public loadingCtrl: LoadingController, public toastCtrl: ToastController, public modalCtrl: ModalController, public globalOverlayProvider: GlobalOverlayProvider, public errorHandler: ErrorHandler, public globalObjectProvider: GlobalObjectProvider) {
+    this.http = http;
+    this.data = null;
+    this.platform = platform;
+    this.student = [];
+    this.platform.ready().then(() => {
+      this.storage = new Storage(SqlStorage);
+    });
 
-    presentLoadingDefault() {
-        let loading = Loading.create({
-            content: 'Please wait...'
-        });
+    //  this.loadStudentInfo();// Pull the student info out of the database right off the bat and store it in a variable
+  }
 
-        this.nav.present(loading);
+  ngOnInit() {
+    //called after the constructor and called  after the first ngOnChanges()
+    this.globalOverlayProvider.presentLoadingMessage("Loading Events...");
+    this.loadEvents();
+  }
 
-        setTimeout(() => {
-            loading.dismiss();
-        }, 5000);
-    }
+  doRefresh() {
+    this.globalOverlayProvider.presentLoadingMessage("Loading Events...");
+    this.loadEvents();
+    // this.nav.setRoot(EventRegPage);
+  }
 
-    doRefresh(refresher) {
-        this.loadEvents();
+  openModal() {
+    let modal = this.modalCtrl.create(ModalsContentPage);
+    modal.present();
+  }
 
-        setTimeout(() => {
-            console.log('Refresh: Async operation has ended');
-            refresher.complete();
-        }, 2000);
-    }
 
-    loadEvents(){
-      this.eventRegService.load()
+  loadEvents() {
+    var now = new Date();
+    var tempDate;
+    this.eventRegService.load()
       .then(data => {
         console.log(data);
-        this.events = data.map(function(postData) {
-            // The .map lets me take the original data and map it to the format the audio player wants
-                    return {
-                      title: postData.acf.event_title,
-                      desc: postData.acf.event_description,
-                      imageURL:  postData.acf.event_image,
-                    }
-               });
+        data.sort(this.comp);
+        console.log(data);
+
+        this.events = data
+          .filter((postData) => {
+            tempDate = new Date(postData.acf.event_date).getTime();
+            if (tempDate < now) {
+              return false;
+            } else {
+              return true;
+            }
+          })
+          .map((postData) => {
+            return {
+              title: postData.acf.event_title,
+              desc: postData.acf.event_description,
+              imageURL: postData.acf.event_image,
+              startDate: postData.acf.event_date,
+              id: postData.id
+            }
+          });
         console.log(this.events);
+        this.globalOverlayProvider.dismissLoading();
+      }, (error) => {
+        //if contains Error: timeout, handle
+        console.log(error)
+        this.globalOverlayProvider.dismissLoading();
       });
+  }
+
+  presentToastMessage(toastMessage) {
+    let toast = this.toastCtrl.create({
+      message: toastMessage,
+      duration: 3000,
+      position: 'top'
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+      this.dismissModal();
+    });
+
+    toast.present();
+  }
+
+  dismissModal() {
+    this.viewCtrl.dismiss();
+  }
+
+
+  submitToSheet(eventTitle, eventID) {
+    //this.loadStudentInfo();
+    this.globalOverlayProvider.presentLoadingMessage("Submitting entry...");
+    var student = this.studentInfoService.getGlobalStudentInfo();
+    var values = "First Name=" + student.firstName +
+      "&Last Name=" + student.lastName +
+      "&Email=" + student.emailAddress +
+      "&Mobile=" + student.phoneNumber +
+      "&Year In School=" + student.yearInSchool +
+      "&Gender=" + student.gender +
+      "&Event=" + eventTitle;
+
+    var url = "https://script.google.com/macros/s/AKfycbzXE56xfKqseHAIcdtWpR2d1H5xZB-VpaFf9XZW6nUpF8x1rEM/exec";
+
+    this.http.get(url + '?' + values)
+      .timeout(35000)
+      .map(res => res.json())
+      .subscribe(
+      (data) => {
+        console.log(data);
+        this.handleSubmissionResponse(data, eventID);
+      },
+      (err) => {
+        console.log(err);
+        this.globalOverlayProvider.dismissLoading();
+        this.errorHandler.handleHTTPError(err);
+      } // Reach here if fails
+      );
+  }
+
+
+  handleSubmissionResponse(data, eventID) {
+    this.globalOverlayProvider.dismissLoading();
+
+    if (data.result == "success") {
+      setTimeout(() => { // Temporary hack since beta 11 won't dismiss the loading overlay when toast pops
+        this.presentToastMessage("Your registration has been successfully submitted!");
+      }, 2000);
+
+      this.studentInfoService.appendEventID(eventID);
+    } else {
+      this.globalOverlayProvider.presentErrorAlertMessage("There was a problem with your submission. Try again, will ya? If the problem keeps up, yell at Phil Lopez or just give up and pray about it :)");
+    }
+  }
+
+  isNotRegistered(eventID) {
+    var eventsArray = [];
+    if (this.isSudentProfileBlank()) {
+      return false;
+    } else {
+      var tempStudentInfo = this.studentInfoService.getGlobalStudentInfo();
+
+      if (!Array.isArray(tempStudentInfo.registeredEvents)) {
+        eventsArray = tempStudentInfo.registeredEvents.split(",");
+      } else {
+        eventsArray = tempStudentInfo.registeredEvents;
+      }
     }
 
-
-    loadStudentInfo() {
-        var values = null;
-        this.platform.ready().then(() => {
-            this.storage.query("SELECT * FROM student").then((data) => {
-                this.student = [];
-                if (data.res.rows.length > 0) {
-                    for (var i = 0; i < data.res.rows.length; i++) {
-                        this.student.push({ firstname: data.res.rows.item(i).firstname, lastname: data.res.rows.item(i).lastname, email: data.res.rows.item(i).email, phone: data.res.rows.item(i).phone });
-
-                        values = "First Name=" + data.res.rows.item(i).firstname +
-                            "&Last Name=" + data.res.rows.item(i).lastname +
-                            "&Email=" + data.res.rows.item(i).email +
-                            "&Mobile=" + data.res.rows.item(i).phone +
-                            "&Year In School=" + data.res.rows.item(i).year +
-                            "&Gender=" + data.res.rows.item(i).gender;
-                        this.formValues = values;
-                    }
-                }
-            }, (error) => {
-                console.log("ERROR -> " + JSON.stringify(error.err));
-            });
-        });
+    if (eventsArray.indexOf(eventID.toString()) < 0) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    submitToSheet(eventTitle) {
-        var values = this.formValues;
-        values += "&Event=" + eventTitle;
-        var url = "https://script.google.com/macros/s/AKfycbzXE56xfKqseHAIcdtWpR2d1H5xZB-VpaFf9XZW6nUpF8x1rEM/exec";
-
-        // var headers = new Headers();
-        // headers.append('Content-Type', 'application/json; charset=utf-8');
-        // this.http.post('https://script.google.com/macros/s/AKfycbzXE56xfKqseHAIcdtWpR2d1H5xZB-VpaFf9XZW6nUpF8x1rEM/exec', values, {
-        //    headers: headers
-        //    })
-        //    .map(res => res.json())
-        //    .subscribe(
-        //     // data => this.saveJwt(data.id_token),
-        //      err => this.logError(err),
-        //      () => console.log('Authentication Complete')
-        //    );
-        console.log("url: " + url);
-        console.log("values: " + values);
-        this.http.get(url + '?' + values)
-            .map(res => res.text())
-            .subscribe(
-            // data => this.randomQuote = data,
-            err => this.logError(err),
-            () => console.log('Success')
-            );
+  isSudentProfileBlank() {
+    if (this.studentInfoService.getGlobalStudentInfo() == "") {
+      return true;
+    } else {
+      return false;
     }
+  }
+
+  formatDateDisplay(event) {
+    var month = event.startDate.substring(0, 2);
+    var day = event.startDate.substring(3, 5);
+    var year = event.startDate.substring(6, 10);
+    var value = this.globalObjectProvider.monthString(month) + " " + day + ", " + year;
+    return value;
+  }
+
+  comp(a, b) {
+    return new Date(a.acf.event_date).getTime() - new Date(b.acf.event_date).getTime();
+  }
+
+}
 
 
+@Component({
+  templateUrl: './build/pages/modals/profile-alert.html',
+  providers: []
+})
+export class ProfileAlertModalPage {
 
-    logError(err) {
-        console.error('There was an error: ' + err);
-    }
+  platform: Platform;
+  http: Http;
+  public student: any; // This is the object used on the page
+  public genderList: any;
+  public yearInSchoolList: any;
+  storage: Storage;
+  formValues: String;
+
+
+  studentForm: ControlGroup;
+  formBuilder: FormBuilder;
+
+  firstName: AbstractControl;
+  lastName: AbstractControl;
+  emailAddress: AbstractControl;
+  phoneNumber: AbstractControl;
+  yearInSchool: AbstractControl;
+  gender: AbstractControl;
+
+
+  constructor(private nav: NavController, private studentInfoService: StudentInfoService, public viewCtrl: ViewController) {
+  }
+
+  ngOnInit() {
+
+  }
+
+  dismissModal() {
+    this.viewCtrl.dismiss();
+  }
+
+
 
 }
